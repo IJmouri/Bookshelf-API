5fbea083bc5051eb549bc137b01be310830ca8a5
Israt-Jahan-Mouri <mouri2018@gmail.com>
2020-11-27 00:58:03 +0600

    updated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/book-management-system', {
+// mongoose.connect('mongodb://127.0.0.1:27017/book-management-system', {
+mongoose.connect('mongodb+srv://bookshelf:!@#$%^&*@cluster0.anx5q.mongodb.net/book-management?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
